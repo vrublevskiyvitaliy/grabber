499429698bb43a068d3358d26e3171ea039b2154
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2016-12-25 00:05:09 +0200

    use 127.0.0.1 rather than localhost

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=grabber_main',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=grabber_main',
             'username' => 'root',
             'password' => '123',
             'charset' => 'utf8',
